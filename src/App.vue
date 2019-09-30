6437ed6d2cf229aca8414280bbaba12e34e92c6d
周哲超 <zhouzhechao@meituan.com>
2019-09-30 17:29:33 +0800

    feat: add git link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
+    <a
+      href="https://github.com/Rabbitzzc/transition-group-key"
+      target="_black"
+    >
+      <img
+        src="../imgs/git.svg"
+        alt=""
+      ></a>
     <header>
-      <h1><code>transition-group key:bug</code></h1>
+      <h1>
+        <code>transition-group key:bug</code>
+      </h1>
     </header>
     <Group></Group>
     <footer>
@@ -26,6 +36,7 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  position: relative;
   text-align: center;
   color: #fff;
   font-size: 1rem;
@@ -50,5 +61,19 @@ body {
 footer {
   height: 30px;
   font-size: 0.5rem;
+}
+
+a {
+  position: absolute;
+  top: 20px;
+  right: 40px;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  margin-left: 10px;
+}
+img {
+  width: 100%;
+  height: 100%;
 }
 </style>
